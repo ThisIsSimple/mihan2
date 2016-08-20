702e1aa27c97c98a08bde4478b6c7022bb45fcc9
cordelia273 <cordelia273@naver.com>
2016-08-20 12:15:10 +0900

    2016/08/20

--- js/index.js
+++ js/index.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   $('.parallax').parallax();
   $(".button-collapse").sideNav();
   $('.dropdown-button').dropdown();
+
+  var options = [
+  {selector: '#board1', offset: 200, callback: function(el) { Materialize.updateTextFields($(el)); } },
+  {selector: '#board2', offset: 200, callback: function(el) { Materialize.fadeInImage($(el)); } } ];
+  Materialize.scrollFire(options);
 });
